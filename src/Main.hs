6ffccf83c9419a52a0a88905b3da63da3134c68d
kaifransson <franssonkai@gmail.com>
2018-08-10 23:33:39 +0200

    add a simulation loop that prints current positions

--- src/Main.hs
+++ src/Main.hs
@@ -13,8 +13,15 @@ main = do
     [nrRobots, directionString] -> do
       let dirs = evalState parseDirectionString directionString
       let sim = makeSimulation (read nrRobots) dirs
-      return ()
+      runSimulation sim
     _ -> invalidArgs
+
+runSimulation :: Simulation -> IO ()
+runSimulation sim = do
+  putStrLn "Current robot positions:"
+  print $ currentRobotPositions sim
+  putStrLn "What do we do now?"
+  putStrLn "Well we haven't implemented that stuff yet, so nothing ..."
 
 invalidArgs :: IO ()
 invalidArgs = putStrLn "Invalid arguments. Usage: HaskellRobots.exe <nr. of robots> <direction string>"
