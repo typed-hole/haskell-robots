1ffc0a952c2df2865bd7f58ab279f3f804e82ad9
kaifransson <franssonkai@gmail.com>
2018-08-10 18:23:51 +0200

    make a State based parser for direction strings

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,4 +1,10 @@
-module Parsing where
+module Parsing
+    ( Direction(..)
+    , parseDirectionString
+    ) where
+
+import           Control.Monad.State
+import           Prelude             hiding (Left, Right)
 
 data Direction
     = Up
@@ -7,3 +13,22 @@ data Direction
     | Right
     deriving (Show, Eq)
 
+parseDirectionString :: State String [Direction]
+parseDirectionString = do
+    s <- get
+    if null s
+        then return []
+        else do
+            let (c:cs) = s
+            let mdir = case c of
+                    '^' -> Just Up
+                    'V' -> Just Down
+                    '<' -> Just Left
+                    '>' -> Just Right
+                    _   -> Nothing
+            case mdir of
+                Nothing    -> return []
+                (Just dir) -> do
+                    put cs
+                    next <- parseDirectionString
+                    return $ dir:next
