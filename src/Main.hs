c490973b138a45589a10ec95555e175bdf3d0274
kaifransson <franssonkai@gmail.com>
2018-08-10 23:24:44 +0200

    add beginnings of CLI

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
+import           Control.Monad.State
+import           System.Environment
+
 import           Parsing
 import           Robots
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  case args of
+    [nrRobots, directionString] -> do
+      let dirs = evalState parseDirectionString directionString
+      let sim = makeSimulation (read nrRobots) dirs
+      return ()
+    _ -> invalidArgs
+
+invalidArgs :: IO ()
+invalidArgs = putStrLn "Invalid arguments. Usage: HaskellRobots.exe <nr. of robots> <direction string>"
